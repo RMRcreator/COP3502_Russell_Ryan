e486e88bdb5c73425a78f5ee31bc64efeb36fa52
RMRcreator <127217042+RMRcreator@users.noreply.github.com>
2023-03-21 17:36:36 -0400

    Update coder.py

--- coder.py
+++ coder.py
@@ -1,28 +1,32 @@
 #Russell Ryan
 
-def encode(password):
-    outer = ''
-    str(password)
-    for element in password:
-        if 0 <= int(element) <= 6:
-            part = int(element) + 3
-            outer += str(part)
+def encode(password):  # encodes a string
+    outer = ''  # sets variable that will store the encoded password as a string
+
+    for element in password:  # goes through every digit in the password
+        if len(element) == 8:
+            part = (int(element) + 3) % 10  # shifts the digit up by three numbers
+            outer += str(part)  # adds the new digit to the encoded password
     return outer
 
 
 def main():
+    stored = None  # stores the encoded password in option 1 for use in option 2
     while True:
-        print("Coding Menu")
-        print("-----------")
-        print("Options:")
-        print("1. Decoder")
-        print("2. Encoder")
-        select = int(input("Select an option: "))
+        print("Menu")  # displays the three options available to users
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        select = int(input("Please enter an option: "))
         if select == 1:
+            passcode = str(input("Please enter your password to encode: "))  # inputs the password to be encoded
+            stored = encode(passcode)  # stores the now-encoded password for later
+            print("Your password has been encoded and stored!")
+        elif select == 2:  # decodes the encoded password
             pass
-        elif select == 2:
-            passcode = str(input("Input password: "))
-            print(encode(passcode))
+        elif select == 3:  # ends the program
+            exit()
 
 
 if __name__ == '__main__':
